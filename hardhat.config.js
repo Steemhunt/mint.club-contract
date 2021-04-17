841a4b962a2770c556e3f518e56a71bad0191660
Sebastian Kim <sydneyitguy@gmail.com>
2021-04-17 11:53:37 +0900

    Implement MintClubToken

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,50 @@
+/// ENVVAR
+// - ENABLE_GAS_REPORT
+// - CI
+// - COMPILE_MODE
+
+const path = require('path');
+const argv = require('yargs/yargs')()
+  .env('')
+  .boolean('enableGasReport')
+  .boolean('ci')
+  .string('compileMode')
+  .argv;
+
+require('@nomiclabs/hardhat-truffle5');
+require('@nomiclabs/hardhat-solhint');
+require('solidity-coverage');
+
+if (argv.enableGasReport) {
+  require('hardhat-gas-reporter');
+}
+
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: '0.8.3',
+    settings: {
+      optimizer: {
+        enabled: argv.enableGasReport || argv.compileMode === 'production',
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 10000000,
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    outputFile: argv.ci ? 'gas-report.txt' : undefined,
+  },
+};
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -9,14 +55,3 @@ task("accounts", "Prints the list of accounts", async () => {
     console.log(account.address);
   }
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.7.3",
-};
-
